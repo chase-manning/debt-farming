b9b3d99f2513af0ada2bb949d822cf5b64cfdbb6
chase-manning <chase@manning.dev>
2022-07-11 00:22:52 +0100

    :sparkles: add token currencies config

---
+++ src/config/tokenCurrencies.ts
@@ -0,0 +1,42 @@
+export const tokensAreSameCurrency = (tokens: string[]): boolean => {
+  for (let i = 0; i < tokenCurrencies.length; i++) {
+    if (!tokenCurrencies[i].includes(tokens[0])) continue;
+    return !tokens.some((token) => !tokenCurrencies[i].includes(token));
+  }
+  return false;
+};
+
+const tokenCurrencies: string[][] = [
+  [
+    "USDC",
+    "USDT",
+    "DAI",
+    "3CRV",
+    "MIM",
+    "FRAX",
+    "sUSD",
+    "TUSD",
+    "alUSD",
+    "mUSD",
+    "OUSD",
+    "BUSD",
+    "LUSD",
+    "USDN",
+    "DOLA",
+    "GUSD",
+    "PUSD",
+    "USDP",
+    "USDD",
+    "FEI",
+    "PAXOS",
+    "PWRD",
+    "HUSD",
+    "USDK",
+    "DUSD",
+    "RSV",
+  ],
+  ["renBTC", "sBTC", "wibBTC", "HBTC", "pBTC", "oBTC", "BBTC", "WBTC"],
+  ["stETH", "alETH", "sETH", "rETH", "wstETH", "ankrETH", "rETH", "ETH"],
+];
+
+export default tokenCurrencies;
