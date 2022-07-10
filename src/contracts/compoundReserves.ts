8f97d8a5dc3c6fcb85a749f5f2e7ac270c1124f0
chase-manning <chase@manning.dev>
2022-07-10 23:22:07 +0100

    :sparkles: add compount reserves view

---
+++ src/contracts/compoundReserves.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { createClient } from "urql";
+import { Reserve } from "./reserves";
+
+const tokensQuery = `
+  query {
+    markets {
+      borrowRate,
+      collateralFactor,
+      supplyRate,
+      underlyingSymbol,
+    }
+  }
+`;
+
+const client = createClient({
+  url: "https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2",
+});
+
+interface ReserveResponse {
+  borrowRate: string;
+  collateralFactor: string;
+  supplyRate: string;
+  underlyingSymbol: string;
+}
+
+const useCompoundReserves = () => {
+  const [reserves, setReserves] = useState<Reserve[]>([]);
+
+  useEffect(() => {
+    const getReserves = async () => {
+      const response = await client.query(tokensQuery).toPromise();
+      setReserves(
+        response.data.markets.map((reserve: ReserveResponse) => {
+          return {
+            symbol: reserve.underlyingSymbol,
+            liquidityRate: Number(reserve.supplyRate) * 100,
+            borrowRate: Number(reserve.borrowRate) * 100,
+            protocol: "Compound",
+          };
+        })
+      );
+    };
+    getReserves();
+  }, []);
+
+  return reserves;
+};
+
+export default useCompoundReserves;
