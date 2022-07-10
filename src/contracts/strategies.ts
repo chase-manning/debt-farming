47c38800efcfcfc7fa2c53775475959f22355dcf
chase-manning <chase@manning.dev>
2022-07-10 16:32:07 +0100

    :sparkles: add strategies view

---
+++ src/contracts/strategies.ts
@@ -0,0 +1,53 @@
+import useReserves, { Reserve } from "./reserves";
+import useYields, { Yield } from "./yields";
+
+interface Strategy {
+  collateral: Reserve;
+  debt: Reserve;
+  yield: Yield;
+  netApy: number;
+}
+
+const useStrategies = (token: string): Strategy[] => {
+  const reserves = useReserves();
+  const yields = useYields();
+
+  const collateral = reserves.find((reserve) => reserve.symbol === token);
+
+  const farmableYields = yields.filter((y: Yield) =>
+    reserves.some(
+      (reserve: Reserve) =>
+        reserve.symbol === y.symbol &&
+        y.apy > reserve.borrowRate &&
+        reserve.symbol !== token
+    )
+  );
+  return farmableYields.map((y: Yield) => {
+    const debt = reserves.find((reserve) => reserve.symbol === y.symbol);
+    if (!collateral || !debt)
+      return {
+        collateral: {
+          symbol: "",
+          liquidityRate: 0,
+          borrowRate: 0,
+          protocol: "",
+        },
+        debt: {
+          symbol: "",
+          liquidityRate: 0,
+          borrowRate: 0,
+          protocol: "",
+        },
+        yield: y,
+        netApy: 0,
+      };
+    return {
+      collateral,
+      debt,
+      yield: y,
+      netApy: collateral.liquidityRate + y.apy - debt.borrowRate,
+    };
+  });
+};
+
+export default useStrategies;
