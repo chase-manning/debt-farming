ce0931dcf1f57ea6219a9db830918798c94840a2
chase-manning <chase@manning.dev>
2022-07-11 16:38:18 +0100

    :bug: some pegged tokens not showing

--- src/config/peggedTokens.ts
+++ src/config/peggedTokens.ts
@@ -1,14 +1,16 @@
 export const isPegged = (tokenA: string, tokenB: string): boolean => {
   if (tokenA === tokenB) return true;
-  if (peggedTokens[tokenA] === tokenB) return true;
-  if (peggedTokens[tokenB] === tokenA) return true;
+  for (let i = 0; i < peggedTokens.length; i++) {
+    if (!peggedTokens[i].includes(tokenA)) continue;
+    return peggedTokens[i].includes(tokenB);
+  }
   return false;
 };
 
-const peggedTokens: Record<string, string> = {
-  ETH: "WETH",
-  CRV: "cvxCRV",
-  FXS: "cvxFXS",
-};
+const peggedTokens: string[][] = [
+  ["ETH", "WETH", "stETH", "STETH"],
+  ["CRV", "cvxCRV"],
+  ["FXS", "cvxFXS"],
+];
 
 export default peggedTokens;
