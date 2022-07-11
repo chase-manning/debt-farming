0f84b66e229d403c316305a786bd6524070da5b1
chase-manning <chase@manning.dev>
2022-07-11 20:25:41 +0100

    :rocket: add convex staking yields

--- src/contracts/yields.ts
+++ src/contracts/yields.ts
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
-import { DEFI_LAMA_YIELDS_URL } from "../app/globals";
-import { tokensAreSameCurrency } from "../config/tokenCurrencies";
-import tokenPrefixes, { TokenPrefix } from "../config/tokenPrefixes";
-import { getVolatileToken } from "../config/volatileTokens";
+import useConvexYields from "./convexYields";
+import useLamaYields from "./lamaYields";
 
 export interface Yield {
   symbol: string;
@@ -10,86 +7,11 @@ export interface Yield {
   apy: number;
 }
 
-interface Response {
-  chain: string;
-  project: string;
-  symbol: string;
-  tvlUsd: number;
-  apy: number;
-  pool: string;
-  apyPct1D: number;
-  apyPct7D: number;
-  apyPct30D: number;
-  projectName: string;
-  stablecoin: boolean;
-  ilRisk: string;
-  exposure: string;
-  predictions: {
-    predictedClass: string;
-    predictedProbability: number;
-    binnedConfidence: number;
-  };
-  audits: string;
-  audit_links: string[];
-  site: string;
-  twitter: string;
-  category: string;
-}
-
-const removeParentheses = (string: string): string => {
-  const parenthesisPosition = string.indexOf("(");
-  if (parenthesisPosition === -1) return string;
-  return string.substring(0, parenthesisPosition - 1);
-};
-
 const useYields = (): Yield[] => {
-  const [yields, setYields] = useState<Yield[]>([]);
+  const lamaYields = useLamaYields();
+  const convexYields = useConvexYields();
 
-  useEffect(() => {
-    const getYields = async () => {
-      const response = await fetch(DEFI_LAMA_YIELDS_URL);
-      const data = await response.json();
-      setYields(
-        data.data
-          .filter(
-            (res: Response) => res.chain === "Ethereum" && res.tvlUsd >= 100_000
-          )
-          .map((res: Response) => {
-            let { symbol } = res;
-            const protocol = res.project;
-
-            // Removing parenthesis from symbol
-            symbol = removeParentheses(symbol);
-
-            // Extracting most volatile asset
-            if (symbol.includes("-")) {
-              const symbols = symbol.split("-");
-              if (tokensAreSameCurrency(symbols)) {
-                const volatileToken = getVolatileToken(symbols);
-                if (volatileToken) symbol = volatileToken;
-              }
-            }
-
-            // Removing redundant prefixes
-            tokenPrefixes.forEach((prefix: TokenPrefix) => {
-              if (protocol !== prefix.protocol) return;
-              if (!symbol.startsWith(prefix.prefix)) return;
-              symbol = symbol.replace(prefix.prefix, "");
-            });
-
-            // Returning the yield
-            return {
-              symbol,
-              protocol,
-              apy: res.apy,
-            };
-          })
-      );
-    };
-    getYields();
-  }, []);
-
-  return yields;
+  return [...lamaYields, ...convexYields];
 };
 
 export default useYields;
