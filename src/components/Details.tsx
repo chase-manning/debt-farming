68c56f7e62cdad8a88196ef4b91c0de84a99fb39
chase-manning <chase@manning.dev>
2022-07-12 18:46:48 +0100

    :sparkles: create details component

---
+++ src/components/Details.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import Strategies from "./Strategies";
+
+import gradient from "../assets/hero/portal.jpg";
+
+const StyledDetails = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  position: relative;
+`;
+
+const Background = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`;
+
+const Details = () => {
+  return (
+    <StyledDetails>
+      <Background src={gradient} alt="Gradient background" />
+      <Strategies />
+    </StyledDetails>
+  );
+};
+
+export default Details;
