099dd36dd77d13e34ab7874e4b3804ff2d9356b6
chase-manning <chase@manning.dev>
2022-07-12 23:29:26 +0100

    :wrench: add tokenlist url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,3 @@
 export const DEFI_LAMA_YIELDS_URL = "https://yields.llama.fi/pools";
 export const DEFI_LAMA_PROTOCOLS_URL = "https://api.llama.fi/protocols";
+export const TOKENLIST_URL = "https://tokens.coingecko.com/uniswap/all.json";
